ea6bcd6696416831e0d6c8ebfcdfe007ae05f171
gary <gary.white@lodgit.com>
2021-10-14 13:00:31 +0200

    code working

--- kataFakeBinary.js
+++ kataFakeBinary.js
@@ -1,21 +1,14 @@
 function fakeBin(x){
     var ans = x.split('')
-    var result;
+    var result = [];
     for (var i = 0; i < ans.length; i++) {
-        if (ans[i] <= 5) {
-            result.push(ans[i])
+        if (ans[i] < 5) {
+            result.push(0)
+        } else {
+            result.push(1)
         }
     }
-    
-    return ans;
-    // for (var i = 0; i < x.Length; i++)
-    //     {
-    //       if (x[i] <= '5')
-    //           ans += '0';
-    //     }  else if (x[i] > 5) {
-    //           ans += '1';
-    //     }
-    //     return ans;
-    }
+    return result.join("");
+}
 
-    console.log(fakeBin('45385593107843568')) // Returns '01011110001100111'
+console.log(fakeBin('45385593107843568')) // Returns '01011110001100111'
